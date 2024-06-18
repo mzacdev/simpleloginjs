8cae17ad0a17705df35eb3984cb753689c8bf49a
mzacdev <mzacdev@gmail.com>
2024-06-18 13:07:22 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Simple in-memory user for authentication
+const user = {
+    username: 'user',
+    password: 'password'
+};
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    if (username === user.username && password === user.password) {
+        res.sendFile(path.join(__dirname, 'views', 'success.html'));
+    } else {
+        res.sendFile(path.join(__dirname, 'views', 'fail.html'));
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
